e5a3e023b86acbdeedffc0b91d2b46c91150297e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-04-12 16:15:25 +0100

    Update config.py

--- config.py
+++ config.py
@@ -29,17 +29,11 @@ class Config:
 
         node = 'https://www.epa.gov/sites/production/files/2018-08/nata2014v2_national_{}'
 
-        strings = {node.format('cancerrisk_by_tract_srcgrp.xlsx'): 'cancerRiskSource',
-                   node.format('cancerrisk_by_tract_poll.xlsx'): 'cancerRiskPollutants',
-                   node.format('resphi_by_tract_srcgrp.xlsx'): 'respiratoryIndexSource',
+        strings = {node.format('cancerrisk_by_tract_poll.xlsx'): 'cancerRiskPollutants',
                    node.format('resphi_by_tract_poll.xlsx'): 'respiratoryIndexPollutants',
-                   node.format('neurhi_by_tract_srcgrp.xlsx'): 'neurologicalIndexSource',
                    node.format('neurhi_by_tract_poll.xlsx'): 'neurologicalIndexPollutants',
-                   node.format('liverhi_by_tract_srcgrp.xlsx'): 'liverIndexSource',
                    node.format('liverhi_by_tract_poll.xlsx'): 'liverIndexPollutants',
-                   node.format('kidnhi_by_tract_srcgrp.xlsx'): 'kidneyIndexSource',
                    node.format('kidnhi_by_tract_poll.xlsx'): 'kidneyIndexPollutants',
-                   node.format('immuhi_by_tract_srcgrp.xlsx'): 'immunologicalIndexSource',
                    node.format('immuhi_by_tract_poll.xlsx'): 'immunologicalIndexPollutants'}
 
         return strings
