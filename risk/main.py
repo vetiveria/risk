a8ac5ddfdf0e0da2ab086495d5ae768d1fb0fe68
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-05-17 23:49:25 +0100

    Update risk/main.py

--- risk/main.py
+++ risk/main.py
@@ -10,56 +10,50 @@ def main():
     directories.cleanup(listof=[configurations.warehouse])
     directories.create(listof=[configurations.warehouse])
 
-    # Example
-    url = 'https://www.epa.gov/sites/production/files/2018-08/nata2014v2_national_neurhi_by_tract_poll.xlsx'
-    descriptor = 'neurologicalIndexPollutants'
-
     strings: dict = configurations.source()
+    for url, descriptor in strings.items():
 
-    for key, value in strings.items():
-        print(key, value)
+        # Hence
+        data = estimates.exc(url=url)
+        logger.info(data.info())
 
-    # Hence
-    data = estimates.exc(url=url)
-    logger.info(data.info())
+        # Drop any unresolved compounds
+        drop = data.columns[data.columns.isin(configurations.unresolved)].to_list()
+        data.drop(columns=drop, inplace=True)
+        logger.info(data.info())
 
-    # Drop any unresolved compounds
-    drop = data.columns[data.columns.isin(configurations.unresolved)].to_list()
-    data.drop(columns=drop, inplace=True)
-    logger.info(data.info())
+        # Update old geographic codes
+        data = updates.exc(blob=data.copy(), identifiers=configurations.identifiers)
 
-    # Update old geographic codes
-    data = updates.exc(blob=data.copy(), identifiers=configurations.identifiers)
+        # Boundaries courtesy of 'total' cells
+        indices = data.columns.get_indexer(data.columns)
+        boundaries = indices[data.columns.str.startswith('total')]
+        start = 1 + boundaries[0]
 
-    # Boundaries courtesy of 'total' cells
-    indices = data.columns.get_indexer(data.columns)
-    boundaries = indices[data.columns.str.startswith('total')]
-    start = 1 + boundaries[0]
+        # A number of the field names are pollutant names, but not all the names are standard names.  The
+        # pollutants function returns a DataFrame of mapped names wherein the field 'chemical' always has
+        # the standard names.  This ensures the retrievability of each pollutant's chemical identification code.
+        toxins = pollutants.exc(compounds=list(data.columns[start:]))
+        logger.info(toxins.info())
 
-    # A number of the field names are pollutant names, but not all the names are standard names.  The
-    # pollutants function returns a DataFrame of mapped names wherein the field 'chemical' always has
-    # the standard names.  This ensures the retrievability of each pollutant's chemical identification code.
-    toxins = pollutants.exc(compounds=list(data.columns[start:]))
-    logger.info(toxins.info())
+        # ... retrieve each pollutant's chemical identification code
+        toxins = toxins.merge(chemicals.exc(), how='left', on='chemical')
+        logger.info(toxins.info())
 
-    # ... retrieve each pollutant's chemical identification code
-    toxins = toxins.merge(chemicals.exc(), how='left', on='chemical')
-    logger.info(toxins.info())
+        # ... the mapping of pollutant field name & chemical identification code; dictionary form
+        mappings = toxins[['field', 'tri_chem_id']].set_index(keys='field', drop=True, inplace=False)\
+            .to_dict(orient='dict')['tri_chem_id']
 
-    # ... the mapping of pollutant field name & chemical identification code; dictionary form
-    mappings = toxins[['field', 'tri_chem_id']].set_index(keys='field', drop=True, inplace=False)\
-        .to_dict(orient='dict')['tri_chem_id']
+        # Hence, rename the pollutant fields
+        data.rename(columns=mappings, inplace=True)
+        logger.info('{}'.format(data.head().iloc[:, :7]))
 
-    # Hence, rename the pollutant fields
-    data.rename(columns=mappings, inplace=True)
-    logger.info('{}'.format(data.head().iloc[:, :7]))
+        # Drop the/any 'total' columns
+        data.drop(columns=data.columns[boundaries].tolist(), inplace=True)
+        logger.info(data.info())
 
-    # Drop the/any 'total' columns
-    data.drop(columns=data.columns[boundaries].tolist(), inplace=True)
-    logger.info(data.info())
-
-    # Save
-    write.exc(data=data, toxins=toxins[['tri_chem_id', 'chemical', 'field']], label=descriptor)
+        # Save
+        write.exc(data=data, toxins=toxins[['tri_chem_id', 'chemical', 'field']], label=descriptor)
 
 
 if __name__ == '__main__':
